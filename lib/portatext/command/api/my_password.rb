368322888954abb735f9f82c99a53d75055fd258
Marcelo Gornstein <marcelog@gmail.com>
2016-01-05 17:13:18 -0300

    adding support for password reset

--- lib/portatext/command/api/my_password.rb
+++ lib/portatext/command/api/my_password.rb
@@ -8,13 +8,31 @@ module PortaText
       # Copyright:: Copyright (c) 2015 PortaText
       # License::   Apache-2.0
       class MyPassword < Base
+        def reset
+          set :reset, true
+        end
+
+        def for_email(email)
+          set :email, email
+        end
+
+        def with_nonce(nonce, new_password)
+          set :nonce, nonce
+          set :new_password, new_password
+        end
+
         def change(old_password, new_password)
           set :old_password, old_password
           set :new_password, new_password
         end
 
         def endpoint(_method)
-          'me/password'
+          return 'me/password' if @args[:reset].nil?
+          @args.delete :reset
+          return 'me/password/reset' if @args[:nonce].nil?
+          nonce = @args[:nonce]
+          @args.delete :nonce
+          "me/password/reset/#{nonce}"
         end
       end
     end
--- test/endpoints/my_password_test.rb
+++ test/endpoints/my_password_test.rb
@@ -7,6 +7,30 @@ module PortaText
       # Copyright:: Copyright (c) 2015 PortaText
       # License::   Apache-2.0
       class MyPassword < PortaText::Test::Helper::CommandTester
+        def test_can_ask_to_reset_password
+          test_command 'me/password/reset', {
+            'email' => 'john@doe.com'
+          } do |client|
+            client
+              .my_password
+              .reset
+              .for_email('john@doe.com')
+              .post
+          end
+        end
+
+        def test_can_reset_password_with_a_nonce
+          test_command 'me/password/reset/a_nonce', {
+            'new_password' => 'new_password4$$'
+          } do |client|
+            client
+              .my_password
+              .reset
+              .with_nonce('a_nonce', 'new_password4$$')
+              .put
+          end
+        end
+
         def test_can_change_password
           test_command 'me/password', {
             'old_password' => 'my_current_password',
